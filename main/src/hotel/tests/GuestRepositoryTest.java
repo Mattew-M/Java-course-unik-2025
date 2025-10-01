76b9f8ae278368b59949854619a7ea4bf536f012
MM <matviymb@gmail.com>
2025-10-01 15:05:25 +0300

    +

--- main/src/hotel/tests/GuestRepositoryTest.java
+++ main/src/hotel/tests/GuestRepositoryTest.java
@@ -4,9 +4,9 @@ import hotel.model.Guest;
 import hotel.repository.GenericRepository;
 import org.junit.jupiter.api.Test;
 
-import static org.testng.AssertJUnit.assertEquals;
-import static org.testng.AssertJUnit.assertFalse;
-import static org.testng.AssertJUnit.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class GuestRepositoryTest {
 
